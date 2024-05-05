047b49e397dab4125f0d31f625eaf1f39c7f083e
G1LD4S <145431000+G1LD4S@users.noreply.github.com>
2024-05-05 11:50:38 +0200

    Update homepage.js

---
+++ front/homepage.js
@@ -0,0 +1,22 @@
+const connexionbtn = document.getElementById("Connexion");
+// const token = localStorage.getItem("token");
+
+const redirection = async () => {
+    const token = localStorage.getItem("token"); 
+
+    if (!token) {
+        return (window.location.href = 'connexion.html');
+    }
+
+    const response = await fetch('http://localhost:3000/getMyProfile', {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+
+    const data = await response.json();
+    console.log(data);
+};
+connexionbtn.addEventListener("click", function() {
+    redirection();
+});
